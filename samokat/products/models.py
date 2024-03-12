aaf4f227d9c921caeb18100ddeaeb95002314102
sp-va <vadis99@ya.ru>
2024-03-12 21:46:58 +0500

    Products.shelf_life field altered to IntegerField

--- samokat/products/models.py
+++ samokat/products/models.py
@@ -7,5 +7,5 @@ class Product(models.Model):
     id: models.UUIDField = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     name: models.CharField = models.CharField()
     description: models.TextField = models.TextField()
-    shelf_life: models.DurationField = models.DurationField()
+    shelf_life: models.IntegerField = models.IntegerField()
     manufacturer: models.CharField = models.CharField()
