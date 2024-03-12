3554be9b94ee0be5b922453746ffd25297e395c6
sp-va <vadis99@ya.ru>
2024-03-12 21:51:51 +0500

    Products.shelf_life may be null

--- samokat/products/models.py
+++ samokat/products/models.py
@@ -7,5 +7,5 @@ class Product(models.Model):
     id: models.UUIDField = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     name: models.CharField = models.CharField()
     description: models.TextField = models.TextField()
-    shelf_life: models.IntegerField = models.IntegerField()
+    shelf_life: models.IntegerField = models.IntegerField(default=None, null=True, blank=True)
     manufacturer: models.CharField = models.CharField()
