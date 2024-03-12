2fe7dd62e084021b085daf6866618462458e0e36
sp-va <vadis99@ya.ru>
2024-03-12 21:07:10 +0500

    Products ModelViewSet

--- samokat/products/views.py
+++ samokat/products/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from serializers import ProductSerializer
+from models import Product
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
