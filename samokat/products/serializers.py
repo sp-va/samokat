c46cd7e7bbb5afaea1f1b65cdb99b3feb04a896b
sp-va <vadis99@ya.ru>
2024-03-12 21:06:53 +0500

    Products serializer

--- samokat/products/serializers.py
+++ samokat/products/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from models import Product
 
 
-class ProductSerializeer(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
