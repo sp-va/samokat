f172e855c78e937082c00b19fe29a1d00b45d7bf
sp-va <vadis99@ya.ru>
2024-03-12 20:10:49 +0500

    Products model

--- samokat/products/models.py
+++ samokat/products/models.py
@@ -1,3 +1,12 @@
+import uuid
+import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id: models.UUIDField = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    name: models.CharField = models.CharField()
+    description: models.TextField = models.TextField()
+    shelf_life: models.DurationField = models.DurationField()
+    manufacturer: models.CharField = models.CharField()
