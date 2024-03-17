280f57b7b8879e6aabf74b7ad9dd77b43cbdd550
sp-va <vadis99@ya.ru>
2024-03-17 21:07:10 +0500

    OrderCart model

--- samokat/carts/models.py
+++ samokat/carts/models.py
@@ -1,3 +1,12 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+from products.models import Product
+from users.models import User
+
+class OrderCart(models.Model):
+    id: models.UUIDField = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    owner: models.ForeignKey = models.ForeignKey(User, on_delete=models.CASCADE)
+    products: models.ManyToManyField = models.ManyToManyField(Product)
+
