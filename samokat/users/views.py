44ae31dd399bb8f0f9d0d241866dc633a3ca0be2
sp-va <vadis99@ya.ru>
2024-03-20 22:41:16 +0500

    Altered user registrations endpoint

--- samokat/users/views.py
+++ samokat/users/views.py
@@ -1,18 +1,10 @@
 from rest_framework.generics import CreateAPIView
-from rest_framework.response import Response
 
-from .models import UserManager
 from .serializers import UserSerializer
-from carts.models import OrderCart
+
 
 
 class RegisterUser(CreateAPIView):
     serializer_class = UserSerializer
-    # def post(self, request):
-    #     serializer_class = UserSerializer(data=request.data)
-    #     serializer_class.is_valid(raise_exception=True)
-    #     serializer_class.save()
-    #     return Response({"new_user": serializer_class.data})
 
-    def perform_create(self, serializer):
-        return OrderCart.objects.create(owner=serializer.email)
+
